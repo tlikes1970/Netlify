d3c206ffb20875d4e5c016c2af6b82634bd8a435
tlikes1970 <likes.travis@gmail.com>
2025-09-19 09:01:21 -0400

    Fix CSP frame-src to allow Firebase auth domain (v27.4)

--- www/js/config.js
+++ www/js/config.js
@@ -27,7 +27,7 @@ export class Config {
         clientId: "",
       },
       app: {
-        version: "27.3",
+        version: "27.4",
         name: "Flicklet - TV & Movie Tracker",
       },
     };
