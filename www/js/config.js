a11eca0931f4587fa6dfd28ce67102837be8d1db
tlikes1970 <likes.travis@gmail.com>
2025-09-19 14:28:45 -0400

    v27.16: Update version numbers after script loading fix
    - Updated version from v27.15 to v27.16
    - Reflects the script loading conflict resolution
    - All version references now consistent across files

--- www/js/config.js
+++ www/js/config.js
@@ -27,7 +27,7 @@ export class Config {
         clientId: "",
       },
       app: {
-        version: "27.14",
+        version: "27.16",
         name: "Flicklet - TV & Movie Tracker",
       },
     };
